28aa9866922555943928172ead213e33bf93b67b
dhav <dhavrutsky@gmail.com>
2019-05-23 14:29:45 -0400

    update

--- ipgrabber.py
+++
@@ -1,6 +0,0 @@
-import discord
-from whitelist import *
-async def ipgrabber(message):
-    await message.channel.send("Yo, watch this funny ass video lmao\nhttps://quickmessage.us/watch.php?id=CI99C5.mp4")
-        
-   
